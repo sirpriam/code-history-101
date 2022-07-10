ac12ecb5f9e4a014080cebbfd3ad2cd4a9c0279d
priambodo.pangestu <priambodo.pangestu@primecash.co.id>
2022-07-10 11:57:07 +0700

    Initial Commit

---
+++ Code History 101/Code_History_101App.swift
@@ -0,0 +1,17 @@
+//
+//  Code_History_101App.swift
+//  Code History 101
+//
+//  Created by Pangestu, Priambodo on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_History_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History 101/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Code History 101
+//
+//  Created by Pangestu, Priambodo on 10/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
